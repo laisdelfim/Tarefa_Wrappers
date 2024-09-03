a8e6bbcb89ede663a29e20b086a8f7bc487bd4ea
brayan@240211 <laisdelfim@gmail.com>
2024-09-03 06:26:19 -0300

    tarefa-ebac

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
         int n1 = 20;
-        short n2 = (short) n1;
+        Double n2 = (double) n1;
         System.out.println(n2);
     }
 }
